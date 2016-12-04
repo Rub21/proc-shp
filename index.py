45a70e2ccd9614133af0b6968daa8221b4464ff5
Rub21 <ruben@mapbox.com>
2016-12-04 16:09:46 -0500

    write shp

--- index.py
+++ index.py
@@ -8,23 +8,15 @@ def __unicode__(self):
 
 geos = []
 with collection("31471050500_face.shp", "r") as input:
-  for geo in input:
-    geos.append(geo)
-    # print shape(geo['geometry'])
-    # print geo['properties']
-    # togeojson.printgeo(geo)
-    # print shape(geo['geometry']).bounds
-
-for geo in geos:
-  name1 = __unicode__(geo['properties']['NM_TIPO_LO']) + ' ' + __unicode__(geo['properties']['NM_TITULO_']) + ' ' + __unicode__(geo['properties']['NM_NOME_LO'])
-  for g in geos:
-    name2 = __unicode__(g['properties']['NM_TIPO_LO']) + ' ' + __unicode__(g['properties']['NM_TITULO_']) + ' ' + __unicode__(g['properties']['NM_NOME_LO'])
-    if geo['properties']['ID'] != g['properties']['ID'] and name1 == name2:
-      # print g['geometry']
-      midline = midleLine.midline(shape(geo['geometry']),shape(g['geometry']))
-      # midline['properties']['name'] = name1
-      togeojson.printgeo(midline)
-
-
-
+  schema = { 'geometry': 'LineString', 'properties': { 'name': 'str' } }
+  with collection("some_buffer.shp", "w", "ESRI Shapefile", schema) as output:
+     for geo in input:
+      geos.append(geo)
+      name = __unicode__(geo['properties']['NM_TIPO_LO']) + ' ' + __unicode__(geo['properties']['NM_TITULO_']) + ' ' + __unicode__(geo['properties']['NM_NOME_LO'])
+      output.write({
+          'properties': {
+          'name': name.title()
+          },
+          'geometry': mapping(shape(geo['geometry']))
+      })
 
